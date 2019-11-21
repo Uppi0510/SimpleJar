84895b38a0d99631b0c2c5e5f4cd17a40b19045e
Uppi0510 <upender.n12@gmail.com>
2019-11-21 15:21:39 +0530

    forth commit

--- src/main/java/FirstMaven/App.java
+++ src/main/java/FirstMaven/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World..!" );
-        System.out.println( "Welcome to Maven Jenkins integration execution..");
+       
         for(int i=0; i<=10;i ++)
          {
               System.out.println("Wellcome to GitHub.... ");
