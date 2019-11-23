1aac63b96477c2eb1f7d0d9a95adc8589547c97e
Uppi0510 <57307714+Uppi0510@users.noreply.github.com>
2019-11-23 11:54:56 +0530

    Update App.java

--- src/main/java/FirstMaven/App.java
+++ src/main/java/FirstMaven/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World..!" );
-        System.out.println( "Welcome to Maven Jenkins integration ..");
+        System.out.println( "Welcome to Maven Jenkins..");
         for(int i=0; i<=10;i ++)
          {
               System.out.println("Wellcome to GitHub.... ");
